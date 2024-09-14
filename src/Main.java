38552d9ea45d275b1f1f8cf4f62dbd5d39c3893c
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-09-14 18:00:15 +0200

    this commit is for creating the code of model client

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import utils.DatabaseConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+        if (dbConnection.getConnection() != null) {
+            System.out.println("Connection established successfully.");
+        } else {
+            System.out.println("Failed to establish connection.");
+        }
     }
 }
--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,84 @@
 package model;
 
 public class Client {
+    private int id;
+    private String nom;
+    private String adresse;
+    private String telephone;
+    private boolean estProfessionnel;
+    private double remise;
+
+
+    public Client() {}
+
+    public Client(int id, String nom, String adresse, String telephone, boolean estProfessionnel, double remise) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.telephone = telephone;
+        this.estProfessionnel = estProfessionnel;
+        this.remise = remise;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public boolean isEstProfessionnel() {
+        return estProfessionnel;
+    }
+
+    public void setEstProfessionnel(boolean estProfessionnel) {
+        this.estProfessionnel = estProfessionnel;
+    }
+
+    public double getRemise() {
+        return remise;
+    }
+
+    public void setRemise(double remise) {
+        this.remise = remise;
+    }
+
+    // Override toString for easy display
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", estProfessionnel=" + estProfessionnel +
+                ", remise=" + remise +
+                '}';
+    }
 }
