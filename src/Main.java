97e334765bc21fcefb17a8d64df3b8654c628d67
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-09-15 11:49:14 +0200

    this commit is for creating the code of main and  fix findeclienbyname

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,44 @@
-import controller.ClientController;
+import controller.ProjectController;
 import service.ClientService;
+import service.ProjectService;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ClientService clientService = new ClientService();
-        ClientController clientController = new ClientController(clientService);
+        ProjectService projectService = new ProjectService();
+        ProjectController projectController = new ProjectController(clientService, projectService);
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\n--- Menu de gestion des clients ---");
-            System.out.println("1. Ajouter un nouveau client");
-            System.out.println("2. Rechercher un client par ID");
-            System.out.println("3. Mettre à jour un client");
-            System.out.println("4. Supprimer un client");
-            System.out.println("5. Afficher tous les clients");
-            System.out.println("6. Quitter");
-            System.out.print("Choisissez une option: ");
-
+            System.out.println("\n=== Menu Principal ===");
+            System.out.println("1. Créer un nouveau projet");
+            System.out.println("2. Afficher les projets existants");
+            System.out.println("3. Calculer le coût d'un projet");
+            System.out.println("4. Quitter");
+            System.out.print("Choisissez une option : ");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    clientController.addClient();
+                    projectController.createProject();
                     break;
+
                 case 2:
-                    clientController.findClient();
+                    projectController.displayAllProjects();
                     break;
+
                 case 3:
-                    clientController.updateClient();
+                    // Implement the cost calculation logic here
                     break;
+
                 case 4:
-                    clientController.deleteClient();
-                    break;
-                case 5:
-                    clientController.displayAllClients();
-                    break;
-                case 6:
-                    System.out.println("Merci d'avoir utilisé le gestionnaire de clients.");
+                    System.out.println("Merci d'avoir utilisé le gestionnaire de projets.");
                     System.exit(0);
                     break;
+
                 default:
                     System.out.println("Option invalide. Veuillez réessayer.");
             }
