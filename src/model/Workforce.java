fa5f9f68e40b372a8b063820a0ce7cb287681ef5
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-09-15 15:11:50 +0200

    this commit is for creating the workforce model

---
+++ src/model/Workforce.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Workforce extends Component {
+    private double tauxHoraire;
+    private double heuresTravail;
+    private double productiviteOuvrier;
+
+    public Workforce() {}
+
+    public Workforce(int id, String nom, double coutUnitaire, double quantite, double tauxTVA, double tauxHoraire, double heuresTravail, double productiviteOuvrier) {
+        super(id, nom, coutUnitaire, quantite, "Main-d'œuvre", tauxTVA);
+        this.tauxHoraire = tauxHoraire;
+        this.heuresTravail = heuresTravail;
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    public double getTauxHoraire() { return tauxHoraire; }
+    public void setTauxHoraire(double tauxHoraire) { this.tauxHoraire = tauxHoraire; }
+    public double getHeuresTravail() { return heuresTravail; }
+    public void setHeuresTravail(double heuresTravail) { this.heuresTravail = heuresTravail; }
+    public double getProductiviteOuvrier() { return productiviteOuvrier; }
+    public void setProductiviteOuvrier(double productiviteOuvrier) { this.productiviteOuvrier = productiviteOuvrier; }
+
+    @Override
+    public String toString() {
+        return "Labor{" +
+                "tauxHoraire=" + tauxHoraire +
+                ", heuresTravail=" + heuresTravail +
+                ", productiviteOuvrier=" + productiviteOuvrier +
+                "} " + super.toString();
+    }
+}
