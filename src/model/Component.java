18b0e7170f2e274364826ffa5e92dcec5a6ae9d3
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-09-15 14:58:57 +0200

    this commit is for creating the model of component

---
+++ src/model/Component.java
@@ -0,0 +1,46 @@
+package model;
+
+public abstract class Component {
+    private int id;
+    private String nom;
+    private double coutUnitaire;
+    private double quantite;
+    private String typeComposant; // Should be "Matériel" or "Main-d'œuvre"
+    private double tauxTVA;
+
+    public Component() {}
+
+    public Component(int id, String nom, double coutUnitaire, double quantite, String typeComposant, double tauxTVA) {
+        this.id = id;
+        this.nom = nom;
+        this.coutUnitaire = coutUnitaire;
+        this.quantite = quantite;
+        this.typeComposant = typeComposant;
+        this.tauxTVA = tauxTVA;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public double getCoutUnitaire() { return coutUnitaire; }
+    public void setCoutUnitaire(double coutUnitaire) { this.coutUnitaire = coutUnitaire; }
+    public double getQuantite() { return quantite; }
+    public void setQuantite(double quantite) { this.quantite = quantite; }
+    public String getTypeComposant() { return typeComposant; }
+    public void setTypeComposant(String typeComposant) { this.typeComposant = typeComposant; }
+    public double getTauxTVA() { return tauxTVA; }
+    public void setTauxTVA(double tauxTVA) { this.tauxTVA = tauxTVA; }
+
+    @Override
+    public String toString() {
+        return "Component{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", coutUnitaire=" + coutUnitaire +
+                ", quantite=" + quantite +
+                ", typeComposant='" + typeComposant + '\'' +
+                ", tauxTVA=" + tauxTVA +
+                '}';
+    }
+}
